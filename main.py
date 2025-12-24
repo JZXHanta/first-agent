1140f412539e351e5da21c4ba92ee1279515a025
JZXHanta <havoyles@gmail.com>
2025-12-23 22:33:03 -0500

    updated main.py

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 model = "gemini-2.5-flash"
-content = "Why is Boot.dev dush a great place to learn backend development?"
+content = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 
 req = client.models.generate_content(model=model, contents=content)
 resp = req.text
 
 
 def main():
+    print(content)
     print(resp)
 
 
