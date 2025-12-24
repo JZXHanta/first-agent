5c7025f9a7a3a07fe6e9734ffc462d5f050d3ea1
JZXHanta <havoyles@gmail.com>
2025-12-23 22:43:23 -0500

    added token data to response

--- main.py
+++ main.py
@@ -13,11 +13,15 @@ content = "Why is Boot.dev such a great place to learn backend development? Use 
 
 req = client.models.generate_content(model=model, contents=content)
 resp = req.text
+prompt_token_count = req.usage_metadata.prompt_token_count
+candidates_token_count = req.usage_metadata.candidates_token_count
 
 
 def main():
-    print(content)
-    print(resp)
+    print("User prompt:", content)
+    print("Response tokens:", candidates_token_count)
+    print("Prompt tokens:", prompt_token_count)
+    print("Response:", resp)
 
 
 if __name__ == "__main__":
